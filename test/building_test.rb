18c4cf66ab226b625335f011cc6c81c384037007
Zachery Bergman <bzach634@gmail.com>
2021-02-17 09:23:51 -0700

    Add building_test

--- test/building_test.rb
+++ test/building_test.rb
@@ -5,4 +5,52 @@ require './lib/apartment'
 require './lib/renter'
 
 class BuildingTest < Minitest::Test
+  def test_it_exists
+    building = Building.new
+
+    assert_instance_of Building, building
+  end
+
+  def test_has_readable_attributes
+    building = Building.new
+
+    assert_equal [], building.units
+    assert_equal [], building.renters
+  end
+
+  def test_add_unit
+    building = Building.new
+
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+
+    assert_equal [unit1, unit2], building.units
+  end
+
+  def test_add_renters
+    building = Building.new
+
+    renter1 = Renter.new("Aurora")
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit1.add_renter(renter1)
+
+    renter2 = Renter.new("Tim")
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    unit2.add_renter(renter2)
+
+    assert_equal ["Aurora", "Tim"], building.renters
+  end
+
+  def test_average_rent
+    building = Building.new
+
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+
+
+    assert_equal 1099.5, building.average_rent
+  end
 end
